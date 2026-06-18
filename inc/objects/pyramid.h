ff941f306e43235e27a0c22156b55972fe7d6757
alice <dacryorrhoea@gmail.com>
2026-06-18 10:36:56 +0300

    o1

--- inc/objects/pyramid.h
+++ inc/objects/pyramid.h
@@ -14,30 +14,26 @@ public:
 
             vertices.Reserve(5);
 
-            // Основание: обход против часовой стрелки (если смотреть сверху)
-            vertices.Add(Vertex(-h, -h, -h)); // 0
-            vertices.Add(Vertex( h, -h, -h)); // 1
-            vertices.Add(Vertex( h, -h,  h)); // 2
-            vertices.Add(Vertex(-h, -h,  h)); // 3
+            vertices.Add(Vertex(-h, -h, -h));
+            vertices.Add(Vertex( h, -h, -h));
+            vertices.Add(Vertex( h, -h,  h));
+            vertices.Add(Vertex(-h, -h,  h));
 
-            // Вершина пирамиды
-            vertices.Add(Vertex(0.0f, h, 0.0f)); // 4
+            vertices.Add(Vertex(0.0f, h, 0.0f));
 
             faces.Reserve(6);
 
-            // Нижнее основание (два треугольника, нормали вниз)
-            faces.Add(Face(0, 2, 1)); // 0-2-1
-            faces.Add(Face(0, 3, 2)); // 0-3-2
+            faces.Add(Face(0, 2, 1));
+            faces.Add(Face(0, 3, 2));
 
-            // Боковые грани (нормали наружу)
             for (int i = 0; i < 4; ++i)
-                faces.Add(Face(i, (i + 1) % 4, 4)); // i -> (i+1)%4 -> 4
+                faces.Add(Face(i, (i + 1) % 4, 4));
 
             object_name = "Pyramid";
             color = 0xFFFF22FF;
         } catch (const std::exception& e) {
             std::cerr << "Exception in Pyramid constructor: "
-                    << e.what() << std::endl;
+                      << e.what() << std::endl;
             throw;
         } catch (...) {
             std::cerr << "Unknown exception in Pyramid constructor"
